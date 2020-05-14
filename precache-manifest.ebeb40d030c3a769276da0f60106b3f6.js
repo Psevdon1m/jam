03cc2d8582649008e5f9d477113533e874fc5a7e
Valentyn <valentine.oleynik@gmail.com>
2020-05-14 10:48:27 +0300

    Updates

---
+++ precache-manifest.ebeb40d030c3a769276da0f60106b3f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ab2145a2ef9cce848504ebcad419446",
+    "url": "/jam/index.html"
+  },
+  {
+    "revision": "45f4b0d3688aeb2b8083",
+    "url": "/jam/static/css/main.7c0d9a2b.chunk.css"
+  },
+  {
+    "revision": "f317b9071a4172fb7e73",
+    "url": "/jam/static/js/2.369d934e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jam/static/js/2.369d934e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45f4b0d3688aeb2b8083",
+    "url": "/jam/static/js/main.47550086.chunk.js"
+  },
+  {
+    "revision": "73e73a9789c00c061192",
+    "url": "/jam/static/js/runtime-main.de551f8f.js"
+  },
+  {
+    "revision": "37be7357a5f8583073df7e0aec787eaa",
+    "url": "/jam/static/media/background_photo_desktop.37be7357.jpg"
+  }
+]);
